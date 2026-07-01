ceb70dd9009c71333e9b178108a298bc59953818
iFriZer0 <aleksandervershok@yandex.ru>
2026-07-01 16:39:24 +0300

    gRPC files began to be generated during build.

--- pku/src/api/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-readonly DIRECTORY="../grpc_ui_service"
-
-cd "$(dirname "$0")" || exit 1
-mkdir -p "$DIRECTORY"
-protoc --cpp_out="$DIRECTORY" --grpc_out="$DIRECTORY" --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` ./pku_service.proto
--- pku/src/build.sh
+++ pku/src/build.sh
@@ -2,5 +2,9 @@
 
 readonly BUILD_DIRECTORY="build"
 
+cd "$(dirname "$0")" || exit 1
 mkdir -p "$BUILD_DIRECTORY"
 cmake -B "$BUILD_DIRECTORY"
+cd "$BUILD_DIRECTORY" || exit 1
+make
+cd ".." || exit 1
