769364345b784b7774472b7d6bdbce508c7528f6
iFriZer0 <aleksandervershok@yandex.ru>
2026-07-02 12:35:26 +0300

    Build became cross-platform.

--- pku/src/build.sh
+++ pku/src/build.sh
@@ -5,6 +5,4 @@ readonly BUILD_DIRECTORY="build"
 cd "$(dirname "$0")" || exit 1
 mkdir -p "$BUILD_DIRECTORY"
 cmake -B "$BUILD_DIRECTORY"
-cd "$BUILD_DIRECTORY" || exit 1
-make
-cd ".." || exit 1
+cmake --build "$BUILD_DIRECTORY"
