2d05f19977d9dc44cfcd8bcca61fb8ab4c555c84
katerina876 <mantra2015@mail.ru>
2026-07-04 22:43:58 +0300

    кнопочки

--- gui/src/graphical_views/views/main/qt_view_pku.cpp
+++ gui/src/graphical_views/views/main/qt_view_pku.cpp
@@ -1,3 +1,5 @@
+#include <QComboBox>
+#include <QTextEdit>
 #include <iostream>
 #include <new>
 #include <QStackedWidget>
@@ -5,15 +7,74 @@
 #include <QVBoxLayout>
 #include <QLabel>
 #include <QString>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QCheckBox>
 #include "qt_view_pku.h"
 
 QtViewPKU::QtViewPKU(QStackedWidget *const stacked_widget) noexcept
     : stacked_widget{stacked_widget}
 {
     central_widget = create_widget();
-    QVBoxLayout *layout = create_v_box_layout(central_widget);
-    layout->addWidget(create_label("РК/ПКУ"));
-    central_widget->setLayout(layout);
+    QVBoxLayout *main_layout = create_v_box_layout(central_widget);
+
+    main_layout->addWidget(create_label("Пульт контроля и управления (РК/ПКУ)"));
+
+    main_layout->addWidget(create_label("Выдача РК:"));
+    QHBoxLayout *rk_layout = new QHBoxLayout();
+
+    QLabel *rk_num_label = create_label("Номер РК:");
+    QLineEdit *rk_num_input = new QLineEdit(central_widget);
+    rk_num_input->setMaximumWidth(60);
+
+    QLabel *rk_time_label = create_label("Длительность (мс):");
+    QLineEdit *rk_time_input = new QLineEdit(central_widget);
+    rk_time_input->setMaximumWidth(80);
+
+    QPushButton *btn_send_rk = new QPushButton("Выдать РК", central_widget);
+
+    rk_layout->addWidget(rk_num_label);
+    rk_layout->addWidget(rk_num_input);
+    rk_layout->addWidget(rk_time_label);
+    rk_layout->addWidget(rk_time_input);
+    rk_layout->addWidget(btn_send_rk);
+    main_layout->addLayout(rk_layout);
+
+    main_layout->addWidget(create_label("Настройка срабатывания ПКУ (1-64):"));
+    QHBoxLayout *cfg_layout = new QHBoxLayout();
+
+    QLabel *chan_label = create_label("Выбор ПКУ:");
+    QComboBox *chan_select = new QComboBox(central_widget);
+    for (int i = 1; i <= 64; ++i) {
+        chan_select->addItem(QString("ПКУ %1").arg(i));
+    }
+
+    QLabel *mode_label = create_label("Режим:");
+    QComboBox *mode_select = new QComboBox(central_widget);
+    mode_select->addItem("0 – Отрицательный импульс");
+    mode_select->addItem("1 – Положительный импульс");
+    mode_select->addItem("2 – Фронт");
+
+    QPushButton *btn_set_mode = new QPushButton("Применить режим", central_widget);
+
+    cfg_layout->addWidget(chan_label);
+    cfg_layout->addWidget(chan_select);
+    cfg_layout->addWidget(mode_label);
+    cfg_layout->addWidget(mode_select);
+    cfg_layout->addWidget(btn_set_mode);
+    main_layout->addLayout(cfg_layout);
+
+    main_layout->addWidget(create_label("Измеренная длительность импульсов ПКУ:"));
+
+    QTextEdit *pku_log = new QTextEdit(central_widget);
+    pku_log->setReadOnly(true);
+    main_layout->addWidget(pku_log);
+
+    QLabel *status_label = create_label("Статус ПКУ: устройство готово к работе");
+    main_layout->addWidget(status_label);
+
+    central_widget->setLayout(main_layout);
 }
 
 void QtViewPKU::show()
