0dae4f2d2e6994fb4c3abd3351e93acae606927a
iFriZer0 <aleksandervershok@yandex.ru>
2026-07-20 22:34:54 +0300

    Unit tests for each command were implemented.

--- pku/src/service/tests/calling_pku_service_tests.py
+++ pku/src/service/tests/calling_pku_service_tests.py
@@ -22,6 +22,7 @@ class CallingPkuServiceTests(unittest.TestCase):
 
     DESCRIPTION_SIZE: int = 120
 
+    patcher: unittest.mock._patch
     service: calling_pku_service.CallingPkuService
     caller: connecting_driver_caller.ConnectingDriverCaller
 
@@ -40,3 +41,108 @@ class CallingPkuServiceTests(unittest.TestCase):
         )
         self.assertTrue(response.success)
         self.assertEqual(response.result_text, "")
+
+    def test_get_hardware_status(self) -> None:
+        self.caller.get_hardware_status.return_value = pku_driver_pb2.StandardResponse(success=True, error_message="")
+        response: pku_service_pb2.CommandResponse = self.service.SendCommand(
+            pku_service_pb2.CommandRequest(command_id=self.Commands.GET_HARDWARE_STATUS.value, command_param=""), None
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(response.result_text, "")
+
+    def test_main_info(self) -> None:
+        self.caller.write_main_information.return_value = pku_driver_pb2.StandardResponse(success=True, error_message="")
+        response: pku_service_pb2.CommandResponse = self.service.SendCommand(
+            pku_service_pb2.CommandRequest(
+                command_id=self.Commands.WRITE_MAIN_INFO.value,
+                command_param=self.DELIMITER.join([
+                    "a" * self.DESCRIPTION_SIZE,
+                    "AA:BB:CC:DD:EE:FF",
+                    "1.2.128.255",
+                    "1.1.1.1",
+                    "01.1.1.1",
+                    "0.2.0.2",
+                    "1",
+                    "4"
+                ])
+            ),
+            None
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(response.result_text, "")
+        self.caller.read_main_information.return_value = pku_driver_pb2.ReadMemoryResponse(
+            success=True,
+            error_message="",
+            data= (
+                b"\x50\x4B\x55\x69"
+                + b"\xdc\x05\x00\x00"
+                + b"\x61" * self.DESCRIPTION_SIZE
+                + b"\xAA\xBB\xCC\xDD\xEE\xFF"
+                + b"\x01\x02\x80\xFF"
+                + b"\x01\x01\x01\x01"
+                + b"\x01\x01\x01\x01"
+                + b"\x00\x02\x00\x02"
+                + b"\x01"
+            )
+        )
+        response = self.service.SendCommand(
+            pku_service_pb2.CommandRequest(command_id=self.Commands.READ_MAIN_INFO.value, command_param=""), None
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(response.result_text, self.DELIMITER.join([
+            "\x50\x4B\x55\x69",
+            "1500",
+            "a" * self.DESCRIPTION_SIZE,
+            "\xAA\xBB\xCC\xDD\xEE\xFF",
+            "\x01\x02\x80\xFF",
+            "\x01\x01\x01\x01",
+            "\x01\x01\x01\x01",
+            "\x00\x02\x00\x02",
+            "1"
+        ]))
+
+    def test_read_pku(self) -> None:
+        self.caller.read_pku.return_value = pku_driver_pb2.ReadPkuResponse(
+            success=True,
+            error_message="",
+            durations_ms=([1, 1, 0, 1] + [0] * 60)
+        )
+        response: pku_service_pb2.CommandResponse = self.service.SendCommand(
+            pku_service_pb2.CommandRequest(
+                command_id=self.Commands.READ_PKU.value, command_param=self.DELIMITER.join(["1", "2", "4", "5"])
+            ),
+            None
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(response.result_text, self.DELIMITER.join(["1", "1", "0", "1"] + ["0"] * 60))
+
+    def test_set_pku_mode(self) -> None:
+        self.caller.set_pku_mode.return_value = pku_driver_pb2.StandardResponse(success=True, error_message="")
+        response: pku_service_pb2.CommandResponse = self.service.SendCommand(
+            pku_service_pb2.CommandRequest(
+                command_id=self.Commands.SET_PKU_MODE.value, command_param=self.DELIMITER.join(["1", "0", "op"])
+            ),
+            None
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(response.result_text, "")
+
+    def test_send_rk_by_index(self) -> None:
+        self.caller.send_rk_by_index.return_value = pku_driver_pb2.StandardResponse(success=True, error_message="")
+        response: pku_service_pb2.CommandResponse = self.service.SendCommand(
+            pku_service_pb2.CommandRequest(
+                command_id=self.Commands.SEND_RK_BY_INDEX.value, command_param=self.DELIMITER.join(["1", "100", "op"])
+            ),
+            None
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(response.result_text, "")
+
+    def test_get_version(self) -> None:
+        self.caller.get_version.return_value = pku_driver_pb2.VersionInfo(version="0.04")
+        response: pku_service_pb2.CommandResponse = self.service.SendCommand(
+            pku_service_pb2.CommandRequest(command_id=self.Commands.GET_VERSION.value, command_param=""), None
+        )
+        self.assertTrue(response.success)
+        self.assertEqual(response.result_text, "0.04")
+
